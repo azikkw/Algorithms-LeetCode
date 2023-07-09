bc9b5342da78934872bf9cb506581a93d5e90ff2
PP2-21B030774 <azat.amenov@gmail.com>
2023-07-09 17:58:31 +0600

    Algorithms 09.07.23 1

---
+++ Stack/car_fleet.py
@@ -0,0 +1,12 @@
+def car_fleet(target, position, speed):
+    pairs = [(p, s) for p, s in zip(position, speed)]
+    pairs.sort(reverse=True)
+
+    stack = []
+
+    for p, s in pairs:
+        stack.append((target - p) / s)
+        if len(stack) >= 2 and stack[-1] <= stack[-2]:
+            stack.pop()
+
+    return len(stack)

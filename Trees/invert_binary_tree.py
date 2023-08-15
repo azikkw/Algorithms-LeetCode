45ab9f6e80dd7df6bd656067111ac4b4f0697620
PP2-21B030774 <azat.amenov@gmail.com>
2023-08-15 16:44:33 +0600

    Algotihms 15.08.2023

---
+++ Trees/invert_binary_tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+def invert_tree(root):
+    if root == None:
+        return root
+
+    root.left, root.right = root.right, root.left
+
+    invert_tree(root.left)
+    invert_tree(root.right)
+
+    return root
